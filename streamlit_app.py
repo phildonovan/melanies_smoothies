9e242d67511f2e29fab5404471ef0d8d5014c691
Phil Donovan <philiprogerdonovan@gmail.com>
2025-06-04 21:20:46 +1200

    Update streamlit_app.py
    TRUE

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ if ingredient_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=TRUE)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
